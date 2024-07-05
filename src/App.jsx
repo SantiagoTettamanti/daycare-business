16ce360d0acad147a3c6ee32ebb13ad60fd9d4df
Santiago Tettamanti <santiago.tettamanti@gmail.com>
2024-07-05 12:49:18 -0500

    added template

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './landing/landing'
+import Navbar from './navbar/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
+      <Navbar />
       <Landing />
     </div>
   );
